1618398c48957e3d211692a2bdbe6682fd68989a
Dylan Wang <wyc6990906@hotmail.com>
2022-07-01 16:30:21 -0600

    test branch working

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,11 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+CREATE TABLE users
+(
+    id   SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255)       NOT NULL
 );
+
+# just a test to github branch stuff
+
