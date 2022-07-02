8152e5335a50082b5c5106e62fc2a82d7acd1654
Derine Banis <d.banis.13@gmail.com>
2022-07-02 13:57:49 -0700

    Add menu_items seed

---
+++ db/seeds/02_menu_items_seed.sql
@@ -0,0 +1,153 @@
+INSERT INTO menu_items(name, description, price, image_url, category)
+VALUES
+(
+'Beef Tenderloin Steak Bowl',
+'Medium rare beef tenderloin steak with balsamic vinegar, wasabi, butter & soy based sauce. With half boiled egg.',
+2100,
+'https://tb-static.uber.com/prod/image-proc/processed_images/ee5f2cadd842ca38fe1b5c4d541d6d2e/859baff1d76042a45e319d1de80aec7a.jpeg',
+'Main'
+),
+(
+  'Eel Hitsumabushi',
+  'Eel fillet cooked in soy flavored sauce served on the rice. We recommend adding the house special broth after you have finished half the potion, so you can enjoy two different two kinds of taste!',
+  2300,
+  'https://tb-static.uber.com/prod/image-proc/processed_images/452d6d5924ab8dbc1de869d98176320f/859baff1d76042a45e319d1de80aec7a.jpeg',
+  'Main'
+),
+(
+  'Chicken Karaage Bowl',
+  'Fried chicken with spicy Japanese mayo. Includes salad.',
+  1700,
+  'https://tb-static.uber.com/prod/image-proc/processed_images/926a8f3cbbcad0e09b1a3363ea0dd691/859baff1d76042a45e319d1de80aec7a.jpeg',
+  'Main'
+),
+(
+  'Pork Kakuni Bowl',
+  'Super slow stewed ultra tender pork belly, Takana pickles, soy marinated kelp topped on rice with our special spicy teriyaki sauce. With half boiled egg.',
+  2000,
+  'https://tb-static.uber.com/prod/image-proc/processed_images/7e2065f8659d2c161f6bb422c803cf8c/859baff1d76042a45e319d1de80aec7a.jpeg',
+  'Main'
+),
+(
+  'Almond Tofu',
+  'Light and refreshing almond tofu with fresh orange sauce',
+  1100,
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/290788034_352047747082961_69026224827241735_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=kh47bfIO6dcAX8rs5rJ&edm=APU89FABAAAA&ccb=7-5&oh=00_AT9DS1wN_xrnzblR2PSy7Ynnr8mEVp2TVM6uUGoKfOUkwg&oe=62C80F08&_nc_sid=86f79a',
+  'Dessert'
+),
+(
+  'Deluxe Seafood Bowl',
+  'Generous portions of stunningly appetizing slices of seafood, – Ahi Tuna, Yellow Tail, Snow Crab, Tai Snapper, Salmon, Ikura and Sweet Shrimp and more – on top of premium Koshihikari Sushi rice, much like a seafood bowl at a fisherman’s wharf in Kanazawa.',
+  3800,
+  'https://www.raisu.ca/wp-content/uploads/2020/10/DX-seafood-Bowl-2-768x960.jpg',
+  'Main'
+),
+(
+  'Seafood Tartar',
+  'Freshly marinated seafood mix 2 kinds of tobikko and salmon caviar on top.',
+  900,
+  'https://www.raisu.ca/wp-content/uploads/2020/10/tartar-768x768.jpg',
+  'Appetizer'
+),
+(
+  'Corn and Brussels Sprouts Karaage',
+  'Deep fried fresh BC corn and brussels sprouts with truffle butter and truffle salt.',
+  1300,
+  'https://www.raisu.ca/wp-content/uploads/2020/10/Corn-and-Brussels-sprouts-768x768.jpg',
+  'Appetizer'
+),
+(
+  'Yuzu Parfait',
+  'Original yuzu gelato, yuzu rare cheese cake sponge cake, yuzu cream on top.',
+  1100,
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/284711472_1294310617764020_7371866270660688927_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=BdBGeeUBcIAAX-AbwFN&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg0OTcxMTA0MzMwNDE4NDE4Nw%3D%3D.2-ccb7-5&oh=00_AT9-S2xZIYnAlmIQnXlORA7e_ZDSWe9qi0Li2ezTFf3H-Q&oe=62C87D45&_nc_sid=30a2ef',
+  'Dessert'
+),
+(
+  'Uni Shooter',
+  'Uni, Ikura, Mountain Potate, Quail Egg, with Dashi Ponzu truffle jelly.',
+  1100
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/286320999_761814561935460_8081945160148255135_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=pjzs9OlYpTQAX-0Vb-i&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg1NjIyNjg2OTM2MDMzNzAwNQ%3D%3D.2-ccb7-5&oh=00_AT8soEhot-e5zlh8RyHroZ1VE3Ec7mxfd6sY9_DAyHVHdw&oe=62C740DA&_nc_sid=30a2ef',
+  'Appetizer'
+),
+(
+  'Plum Wine Sangria',
+  'Original plum wine sangria pitcher🍹',
+  3000,
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/280958885_1038420706768657_2448675490488516074_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=zA2rSFnczRUAX8bKT9q&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjgzOTIxMDAxNzQ3MzI2MTk1Mw%3D%3D.2-ccb7-5&oh=00_AT-yGv4amuQkBWy0QHmCo2W_yPrSBOplQm3lhk4K9VIhwQ&oe=62C6C629&_nc_sid=30a2ef',
+  'Drink'
+),
+(
+  'Lemon Sour',
+  'Splash Lemon Sour Pitcher🍋',
+  2000,
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/282031039_165345869240217_4973407023335783998_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=fSwPrUPZzIMAX9ZaptM&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg0MjEzNzU5OTg2NDg0MTU1OA%3D%3D.2-ccb7-5&oh=00_AT-EiULjmxDQ24_RDwJ7U9HhoIJjZJW2l16dHAGQlWzv6Q&oe=62C6E806&_nc_sid=30a2ef',
+  'Drink'
+),
+(
+  'Sea Urchin Donabe Rice',
+  'Sea urchin and snow crab and inure, cooked with original broth',
+  2800,
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/277855182_132738175994218_7732951601405733990_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=jSVyjZLfBtkAX9fJT22&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjgxMDI2Nzc5NzcwMDc4NTQ2NQ%3D%3D.2-ccb7-5&oh=00_AT80tU-a7ZHFYdC5GliG3WtvxWmnuHWUKj6QvTwQ_R3Y4A&oe=62C83C65&_nc_sid=30a2ef',
+  'Main'
+),
+(
+  'Snow Crab on the Egg',
+  'Snow crab and salmon caviar on the top of the boiled egg.',
+  900,
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/275008201_157124476733933_1818323469237537019_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=2GSniL5_dBwAX8cjzfp&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc4NTYxMDgzMjE0ODI3MDgzOQ%3D%3D.2-ccb7-5&oh=00_AT8uyX8uOnxfHQ8EFkc4XgWLIMfR52U0w3g1lSAJrW1ytg&oe=62C7E342&_nc_sid=30a2ef',
+  'Appetizer'
+),
+(
+  'Seafood Bara Chirashi Bowl',
+  'Fresh seafood cubes are festively spread on top of original sushi rice mix with root vegetables and Shiitake mushroom.',
+  2700,
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/271393984_427182582475646_3264541553577865951_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=13eMe1jUfbYAX-a3aMs&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc0NjEzMjM0NDMwMDQzNzQwMQ%3D%3D.2-ccb7-5&oh=00_AT_spiyz9iW-QIIJg-rKV0GiZSKZPBEyUcHCEtuaIvl7lg&oe=62C7A8FA&_nc_sid=30a2ef',
+  'Main'
+),
+(
+  '100% Wagyu Croquette',
+  '100% KOBE Wagyu beef croquette made with original panic breading. Served with Takana Tartar sauce and Tonkatsu sauce.'
+  1300,
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/289303384_729695428369625_372057442968528469_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=35_ScM46Ru0AX_wZRv3&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg2NDkyNTYwMDY5MTc4MTM2MQ%3D%3D.2-ccb7-5&oh=00_AT99RxJ0XOYkESC0HIPF3CoeTA4a0zTXWbMhwK_cSL7Ohg&oe=62C87635&_nc_sid=30a2ef',
+  'Appeztizer'
+),
+(
+  'Hell Chicken Karrage',
+  'Signature chicken karrage tossed in hell sauce. (Original spicy sauce)',
+  1300,
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/290517060_119803380754624_2822404132614588902_n.jpg?stp=dst-jpg_e35_p240x240&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=YE7bekZrBigAX-jXAIY&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg3MDkwMzIwMTI3ODY5NTcyOA%3D%3D.2-ccb7-5&oh=00_AT-AXCnsL4MLxw935oYx_rL0MiH5ycLbGN8Piw8BqEc-sw&oe=62C7539B&_nc_sid=30a2ef',
+  'Appetizer'
+),
+(
+  'Miso Port Cutlet',
+  'Pork cutlet with miso demi glace sauce. Soft boiled egg and grilled vegetables.',
+  2400,
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/289550110_2026103340911239_7214876988580597453_n.jpg?stp=dst-jpg_e35_s240x240&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=zANNceDEUB8AX-cm_Jh&tn=gCSoVS2xp048EH0-&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg2Nzc1Njg2MTg0ODYyMDk2Ng%3D%3D.2-ccb7-5&oh=00_AT_HNswrnnxnH3P33bwXmBBHnL31XsZihHlEkahL3TRISg&oe=62C802CF&_nc_sid=30a2ef',
+  'Main'
+),
+(
+  'Snow Crab and Ikura Hitsumabushi',
+  'Snow crab and ikura served on top of rice in a sizzling hot stone bowl. For the second half of the meal, pour the lightly flavored dashi soup to blend and savor all the deliciousness to the last drop.',
+  2200,
+  'https://instagram.fyvr3-1.fna.fbcdn.net/v/t51.2885-15/284657043_158335590020805_2099053382286761405_n.jpg?stp=dst-jpg_e35_p750x750_sh0.08&_nc_ht=instagram.fyvr3-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=uBlBbNgVrKMAX90r0jC&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg0ODk4MDg5MjAyNzAzMTQzMA%3D%3D.2-ccb7-5&oh=00_AT_dczsIlzrAW-rNwr1FWn34BouHChx8vJXWsxpi2gXiHA&oe=62C70032&_nc_sid=30a2ef',
+  'Main'
+),
+(
+  'Green Tea',
+  'Green Tea in a bottle.',
+  350,
+  'https://tb-static.uber.com/prod/image-proc/processed_images/439289d48cc94c59d68b55c3cb3a6235/859baff1d76042a45e319d1de80aec7a.jpeg',
+  'Drink',
+),
+(
+  'Coca-Cola',
+  'Coca-cola in a bottle.',
+  350,
+  'https://images.unsplash.com/photo-1567103472667-6898f3a79cf2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+  'Drink'
+)
+
+
+
+
