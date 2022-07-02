31e7d23803b7cd3b412d22c4a87b452cca99e013
Derine Banis <d.banis.13@gmail.com>
2022-07-02 14:37:35 -0700

    Add orders database schema

---
+++ db/schema/03_orders.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS orders CASCADE;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  customer_id INT NOT NULL REFERENCES users.id,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  accepted_at TIMESTAMP NOT NULL DEFAULT now(),
+  completed_at TIMESTAMP
+);
