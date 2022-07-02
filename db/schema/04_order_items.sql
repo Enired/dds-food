f1c8096c0c84899af59f61af7f39a693b00fa1cb
Derine Banis <d.banis.13@gmail.com>
2022-07-02 14:48:39 -0700

    Update schema of order_items database.

--- db/schema/04_order_items.sql
+++ db/schema/04_order_items.sql
@@ -1,10 +1,6 @@
 DROP TABLE IF EXISTS order_items CASCADE;
 CREATE TABLE order_items (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  description TEXT,
-  price INT NOT NULL,
-  image_url  VARCHAR(913),
-  category VARCHAR(255),
-  is_active BOOLEAN DEFAULT TRUE
+  order_id INT NOT NULL REFERENCES orders.id,
+  menu_item INT NOT NULL REFERENCES menu_items.id,
+  quantity INT DEFAULT 1
 );
