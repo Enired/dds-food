977134beb4a8b516049b8d422fe04d0c58c9b9b2
Derine Banis <d.banis.13@gmail.com>
2022-07-02 14:13:38 -0700

    Add users seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,9 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users(is_owner, first_name, last_name, email, password, phone_number)
+VALUES
+(false, 'Dylan', 'Wang', 'wyc6990906@dds.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(778)123-4567'),
+(false, 'Derine', 'Banis', 'enired@dds.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(778)234-5671'),
+(false, 'Saad', 'Siddiq', 'saadsidd@dds.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(778)345-6712'),
+(false, 'Octavio', 'Silva', 'octane@legends.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(604)123-4567'),
+(false, 'Kaleb', 'Cross', 'revenant@legends.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(604)234-5671')
+(true, 'Kairi', 'Imahara', 'valk@legends.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(604)345-6712')
+
