1e69794cb9d0103f21b1daaf6350e814918f9249
Derine Banis <d.banis.13@gmail.com>
2022-07-03 15:43:29 -0700

    Add queries and routes for displaying the menu as a whole.

--- routes/menu_items.js
+++ routes/menu_items.js
@@ -2,71 +2,88 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-/**
- * B - GET menu_items
- * R - GET menu_items/:id
- * E - POST menu_items/:id
- * A - POST menu_items
- * D - DELETE menu_items/:id/delete
- *
- *
- *
- *
-*/
-
-  getAllMenuItems = (db) => {
-    db.query(`SELECT * FROM menu_items`)
+  getAllMenuItems = (db, res) => {
+    db.query(`SELECT * FROM menu_items ORDER BY menu_items.category`)
       .then((data)=>{
         const menuItems = data.rows;
+        menuItems.forEach(item=>{console.log(item.id)})
         res.json({menuItems});
       })
       .catch((err) => {
         res
           .status(500)
           .json({ error: err.message });
+        })
+      }
+
+  getMenuItemsCategory = (db, res, category) => {
+    const queryParams = [category]
+    db.query(
+      `
+      SELECT *
+      FROM menu_items
+      WHERE menu_items.category = $1
+      `, queryParams
+    )
+    .then((data)=>{
+      const menuItems = data.rows;
+      res.json({menuItems});
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: err.message });
       })
   }
 
-  getSpecificMenuItems = (db, options) => {
-    //Options should have name, category
-    const params = []
-    let query = `
-    SELECT menu_items.id
-    FROM menu_items
-    `;
-
-    if(options.name){
-      params.push(`%${options.name}%`);
-      query += `WHERE name LIKE $${params.length} `;
-    }
-
-    if(options.category){
-      params.push(`${options.category}`);
-      if(params.length === 1){
-        query += `WHERE menu_items.category = $${params.length} `;
-      }
-      else{
-        query += `AND menu_items.category = $${params.length} `;
-      }
-    }
-
-    db.query(query, params)
-      .then((data)=>{
-        const menuItems = data.rows;
-        res.json({menuItems});
-      })
-      .catch((err) => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      })
-  }
-
-
-  router.get('/', (req, res) =>{
-    getAllMenuItems(db);
+  router.get('/category/:category', (req, res) => {
+    getMenuItemsCategory(db, res, req.params.category);
   })
 
-  router.get('/:id')
+  router.get('/', (req, res) =>{
+    getAllMenuItems(db, res);
+  })
 
+  return router;
 }
+/////////////////////////
+// IGNORE THIS FOR NOW //
+/////////////////////////
+
+
+
+// getSpecificMenuItems = (db, options, res) => {
+//   //Options should have name, category
+//   const params = []
+//   let query = `
+//   SELECT menu_items.id
+//   FROM menu_items
+//   `;
+
+//   if(options.name){
+//     params.push(`%${options.name}%`);
+//     query += `WHERE name LIKE $${params.length} `;
+//   }
+
+//   if(options.category){
+//     params.push(`${options.category}`);
+//     if(params.length === 1){
+//       query += `WHERE menu_items.category = $${params.length} `;
+//     }
+//     else{
+//       query += `AND menu_items.category = $${params.length} `;
+//     }
+//   }
+
+//   db.query(query, params)
+//     .then((data)=>{
+//       const menuItems = data.rows;
+//       res.json({menuItems});
+//     })
+//     .catch((err) => {
+//       res
+//         .status(500)
+//         .json({ error: err.message });
+//     })
+// }
+// router.get('/:id')
