7173391a6053351c10475fb296e48d30f387218b
Derine Banis <d.banis.13@gmail.com>
2022-07-04 13:45:42 -0700

    Change schema and seeds for orders

--- db/schema/03_orders.sql
+++ db/schema/03_orders.sql
@@ -3,6 +3,6 @@ CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   customer_id INT NOT NULL REFERENCES users(id),
   created_at TIMESTAMP NOT NULL DEFAULT now(),
-  accepted_at TIMESTAMP NOT NULL DEFAULT now(),
-  completed_at TIMESTAMP
+  accepted_at TIMESTAMP DEFAULT NULL,
+  completed_at TIMESTAMP DEFAULT NULL
 );
