2f48417fb54d634ed76289a17263466697a0b41b
Derine Banis <d.banis.13@gmail.com>
2022-07-03 22:47:55 -0700

    Refactor for tabular view by category

--- routes/menu_items.js
+++ routes/menu_items.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
+
+  let vars = {}
   getAllMenuItems = (db, res) => {
+    // ALL ITEMS QUERY
     db.query(`
     SELECT *
     FROM menu_items
@@ -14,45 +17,78 @@ module.exports = (db) => {
       WHEN menu_items.category = 'Drink' THEN 4
       END ASC`
       )
-      .then((data)=>{
-        const menuItems = data.rows;
-        console.log(menuItems.length)
-        const vars = {menuItems}
-        res.render('index', vars);
-      })
-      .catch((err) => {
-        res
-          .status(500)
-          .json({ error: err.message });
-        })
-      }
-
-  getMenuItemsCategory = (db, res, category) => {
-    const queryParams = [category]
-    db.query(
-      `
-      SELECT *
-      FROM menu_items
-      WHERE menu_items.category = $1
-      `, queryParams
-    )
     .then((data)=>{
       const menuItems = data.rows;
-      const vars = {menuItems}
-      res.render('index', vars);
+      vars.menuItems = menuItems
     })
+    // DRINKS QUERY
+    .then(()=>{
+      return db.query(
+        `
+        SELECT *
+        FROM menu_items
+        WHERE menu_items.category = 'Drink'
+        `,
+      )
+    })
+    .then((data)=>{
+      const drinks = data.rows;
+      vars.drinks = drinks
+    })
+    // APPY QUERY
+    .then(()=>{
+      return db.query(
+        `
+        SELECT *
+        FROM menu_items
+        WHERE menu_items.category = 'Appetizer'
+        `,
+      )
+    })
+    .then((data)=>{
+      const appetizers = data.rows;
+      vars.appetizers = appetizers
+    })
+    // Main Dishes Query
+    .then(()=>{
+      return db.query(
+        `
+        SELECT *
+        FROM menu_items
+        WHERE menu_items.category = 'Main'
+        `,
+      )
+    })
+    .then((data)=>{
+      const mains = data.rows;
+      vars.mains = mains
+    })
+    // Main Dishes Query
+    .then(()=>{
+      return db.query(
+        `
+        SELECT *
+        FROM menu_items
+        WHERE menu_items.category = 'Dessert'
+        `,
+      )
+    })
+    .then((data)=>{
+      const desserts = data.rows;
+      vars.desserts = desserts
+    })
+    //FINAL RENDER
+    .then(
+      res.render('index', {menuItems: vars.menuItems, drinks : vars.drinks, appetizers : vars.appetizers, mains : vars.mains, desserts: vars.desserts})
+    )
     .catch((err) => {
       res
         .status(500)
         .json({ error: err.message });
       })
-  }
+    }
 
-  router.get('/category/:category', (req, res) => {
-    getMenuItemsCategory(db, res, req.params.category);
-  })
-
-  router.get('/', (req, res) =>{
+    router.get('/', (req, res) =>{
     getAllMenuItems(db, res);
   })
 
@@ -61,6 +97,33 @@ module.exports = (db) => {
 /////////////////////////
 // IGNORE THIS FOR NOW //
 /////////////////////////
+
+  // getMenuItemsCategory = (db, res, category) => {
+  //   const queryParams = [category]
+  //   db.query(
+  //     `
+  //     SELECT *
+  //     FROM menu_items
+  //     WHERE menu_items.category = $1
+  //     `,
+  //     queryParams
+  //   )
+
+  //   .then((data)=>{
+  //     const menuItems = data.rows;
+  //     const vars = {menuItems}
+  //     res.render('index', vars);
+  //   })
+  //   .catch((err) => {
+  //     res
+  //       .status(500)
+  //       .json({ error: err.message });
+  //     })
+  // }
+
+  // router.get('/category/:category', (req, res) => {
+  //   getMenuItemsCategory(db, res, req.params.category);
+  // })
 
 
 
