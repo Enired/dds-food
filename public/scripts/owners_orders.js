b0801a41d303073c4e10dc14df275349356b4fea
Derine Banis <d.banis.13@gmail.com>
2022-07-06 13:00:57 -0700

    Add change made by team member

--- public/scripts/owners_orders.js
+++ public/scripts/owners_orders.js
@@ -8,7 +8,7 @@ const sendETAlert = (time) =>{
 
 }
 
-const test = () =>{
+const smsControls = () =>{
 
   $('.send-time-estimate').each(function(_, element){
     $(this).on('click', function(){
@@ -24,6 +24,24 @@ const test = () =>{
   })
 }
 
+const addTaxToSubtotal = (subtotal) => {
+  const total = Math.floor(subtotal*1.10).toFixed(2)
+  return total
+}
+
+const showTotalWithTax = () => {
+  $('.order-total').each(function(){
+    const subtotal = Number($(this).text())
+    const totalWithTax = addTaxToSubtotal(subtotal);
+    $(this).text(totalWithTax)
+
+  }
+
+)
+
+
+}
 $(document).ready(function () {
-  test();
+  smsControls();
+  showTotalWithTax();
 })
