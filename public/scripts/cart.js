34111a13db99c542951231d549d4fe99864c50d2
Saad Siddiq <siddiq.saad@gmail.com>
2022-07-06 22:22:52 -0500

    added cart query to populate /cart page from db

---
+++ public/scripts/cart.js
@@ -0,0 +1,15 @@
+// Script for /cart that requests menu items based on cookie
+
+// (function($) {
+
+//   $(document).ready(function() {
+
+//     $.get({
+//       url: '/cart/items',
+//       data: getCartCookie(),
+//       success: () => console.log('the GET was successful!')
+//     });
+
+//   });
+
+// })(jQuery);
